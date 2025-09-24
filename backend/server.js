a96338cf484d95b183fd8ab7e75c345a53773cf9
Gayanand <gayanand.patel@gmail.com>
2025-09-24 06:53:04 +0530

    minor change

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,9 @@ const db = new pg.Client({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect();
