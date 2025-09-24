83c6f8e8721f4c389825fd670805c3a7bbf6f3b0
Gayanand <gayanand.patel@gmail.com>
2025-09-25 00:38:02 +0530

    final code

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export const yourUser = 'postgres';
-export const yourHost = 'localhost';
-export const yourDatabase = 'booknote';
-export const yourPassword = 'Gnpatel651234';
-export const yourPort = 5432;
